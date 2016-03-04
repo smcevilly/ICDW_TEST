0d4b72f167ebc6be0b6025a4f3d5cadcc3884509
smcevilly <smcevilly@gmail.com>
2016-03-04 15:06:09 -0500

    First Attempt

---
+++ Database1/Database1/dbo/Tables/PVH_Allocation_02233016.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PVH_Allocation_02233016] (
+    [Stmt_id]    VARCHAR (50) NULL,
+    [Payment_ID] VARCHAR (50) NULL
+);
+
---
+++ Database1/Database1/dbo/Tables/pvh_scott_allocation.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[pvh_scott_allocation] (
+    [Payment_ID] INT    NOT NULL,
+    [stmt_id]    BIGINT NULL
+);
+
